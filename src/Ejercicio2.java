d8a55fa9aa9ee983da026b26d2fbc14797edd6c3
Karen Gutierrez <karenlisethgutierrezlopez@gmail.com>
2026-08-23 22:53:17 -0500

    Ejercicio2 (Método void con parámetros)El ejercicio esta casi acabado

---
+++ src/Ejercicio2.java
@@ -0,0 +1,10 @@
+public class Ejercicio2 {
+    public static void saludarEstudiante(String nombre, int edad) {
+
+        System.out.println("Hola " + nombre);
+        System.out.println("Tienes " + edad + " años");
+    }
+
+    public static void main(String[] args) {
+    }
+}
