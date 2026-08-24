90b2a392bb80c343cf20a8854fb24a354a743102
Karen Gutierrez <karenlisethgutierrezlopez@gmail.com>
2026-08-23 23:40:17 -0500

    Ejercicio7(Metodo con arreglo como parámetro)Hasta ahora solo tengo mi ciclo y mi arreglo

---
+++ src/Ejercicio7.java
@@ -0,0 +1,9 @@
+public class Ejercicio7 {
+    public static double CalcularPromedioClase(double[] notas) {
+
+        double suma = 0;
+        for (int i = 0; i < notas.length; i++) {
+            suma += notas[i];
+        }
+    }
+}
