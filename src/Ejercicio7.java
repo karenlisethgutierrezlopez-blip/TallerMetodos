26c023b7e0348125202e9ba3b9e315b8bbbbf643
Karen Gutierrez <karenlisethgutierrezlopez@gmail.com>
2026-08-23 23:47:18 -0500

    Ejercicio7(Metodo con arreglo como parámetro)ya coloque las notas del arreglo y acabe

--- src/Ejercicio7.java
+++ src/Ejercicio7.java
@@ -5,5 +5,12 @@ public class Ejercicio7 {
         for (int i = 0; i < notas.length; i++) {
             suma += notas[i];
         }
+        return suma / notas.length;
+    }
+
+    public static void main(String[] args) {
+        double[] notas = {5.0, 3.5, 2.0, 4.5};
+        double promedio = CalcularPromedioClase(notas);
+        System.out.println("Promedio de clases es de "+promedio);
     }
 }
