0a2b4174dc3a8c8842aa469308d8fd3c99513d56
Karen Gutierrez <karenlisethgutierrezlopez@gmail.com>
2026-08-23 23:07:45 -0500

    Ejercicio3 (Método con retorno numérico)determinando variables necesarias

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Ejercicio3 {
     public static double CalcularArea(double base, double altura){
         double area = base*altura;
         return area;
+    }
+    public static void main(String[] args) {
+        double resultado = CalcularArea(12.5,23);
+
+        System.out.println("El area del triangulo es de " +resultado);
     }
 }
