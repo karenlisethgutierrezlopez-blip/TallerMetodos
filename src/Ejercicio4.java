1bb5e4ef85428e7a0504eddaa9895bff255d3965
Karen Gutierrez <karenlisethgutierrezlopez@gmail.com>
2026-08-23 23:10:52 -0500

    Ejercicio4(metodo con retorno booleano)cree la condicion

---
+++ src/Ejercicio4.java
@@ -0,0 +1,9 @@
+public class Ejercicio4 {
+    public static boolean esPar(int numero){
+        if (numero%2==0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
